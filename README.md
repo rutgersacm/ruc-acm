2612ec93ff85ca1688e588b64685affa476c36df
Chris Till <cpaultill@gmail.com>
2020-01-23 22:23:57 -0500

    Add flask files and some basic templates (index, officers, events)

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # ruc-acm.github.io
 Repository for the RUC ACM Chapter website. - Webmaster chri55
 
-This site uses Jekyll. 
+### Development
+To install the requirements and run the development server:
+```bash
+$ pip install -r requirements.txt
+$ flask run
+```
---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from app import app
+from flask import render_template, url_for
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/officers')
+def officers():
+    return render_template('officers.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
