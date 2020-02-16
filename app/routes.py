61b3001aedbd29276ff85bed0af2593c64d1226a
Chris Till <cpaultill@gmail.com>
2020-02-16 12:27:56 -0500

    Add new routes/templates and calendar functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,25 @@
 from app import app
+import csv
 from flask import render_template, url_for
+
+
+## Example loading data in from a file,
+## to pass it into the achievements template.
+achievementsList = []
+with open('./app/static/data/test-achievement.csv', 'r') as achievementfile:
+    file_reader = csv.reader(achievementfile, delimiter=',', quotechar='"')
+    firstRow = False
+    for row in file_reader:
+        if not firstRow:
+            firstRow = True
+            continue
+        achievementsList.append({
+        'awardName':row[0],
+        'name':row[1],
+        'description':row[2]
+        })
+
+print(achievementsList)
 
 @app.route('/')
 @app.route('/index')
@@ -17,4 +37,12 @@ def events():
 
 @app.route('/achievements')
 def achievements():
-    return render_template('achievements.html')
+    return render_template('achievements.html', achievementsList=achievementsList)
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/photos')
+def photos():
+    return render_template('photos.html')
